c0ec6da6354770cd1f9427d3dd40c94deb502582
JMSBRN <zakhavai@gmail.com>
2023-06-19 17:57:37 +0300

    fix: updated styles for Admin list and Layout

--- admin/amin-list/AdminList.tsx
+++ admin/amin-list/AdminList.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image';
 import React from 'react';
 import styles from './adminList.module.scss';
 import { useGetList } from 'react-admin';
+import userIcon from '../../public/svgs/Icon_User.svg';
+import verifyIcon from '../../public/svgs/Verify.svg';
 
 const AdminList = () => {
   const {
@@ -8,7 +11,10 @@ const AdminList = () => {
     tableStyle,
     tableHeader,
     rowStyle,
-    tableButton
+    tableButton,
+    getNameBtnText,
+    imagesContainer,
+    veriFyImage
   } = styles;
   const { data } = useGetList('contacts');
 
@@ -22,7 +28,22 @@ const AdminList = () => {
 
             <div className={rowStyle} key={el.id}>
               <div className={tableButton}>
-                <button>Get the name</button>
+                <button>
+                  <div className={imagesContainer}>
+                  <Image 
+                    className={veriFyImage}
+                    width={12}
+                    src={verifyIcon}
+                    alt=" verify arrow in green bg"
+                    />
+                  <Image width={20} height={20} alt={'logo user with arrow'}
+                  src={userIcon}
+                  />
+                  </div>
+                   <div className={getNameBtnText}>
+                    Get the name
+                    </div>
+                  </button>
               </div>
               <div>{el.job_title}</div>
               <div>{el.industry}</div>
